ce699b5a879b43d69eade00df47a2dbab9687476
Permana <sdharma721@gmail.com>
2026-03-16 19:16:44 +0700

    refactor(pair): introduce generics for Pair class

--- Java Generic/src/_02_GenericClass/Pair.java
+++ Java Generic/src/_02_GenericClass/Pair.java
@@ -1,4 +1,27 @@
 package _02_GenericClass;
 
-public class Pair {
+public class Pair<T, U> {
+    private T first;
+    private U second;
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+
+    public void setSecond(U second) {
+        this.second = second;
+    }
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
 }
