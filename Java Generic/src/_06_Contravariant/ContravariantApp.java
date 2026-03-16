4afebda4cd9e44683fd658ed0fcb5317171b49c5
Permana <sdharma721@gmail.com>
2026-03-16 23:22:45 +0700

    refactor(contravariant): Introduce contravariance to MyData generic class using supertype and subtype

--- Java Generic/src/_06_Contravariant/ContravariantApp.java
+++ Java Generic/src/_06_Contravariant/ContravariantApp.java
@@ -1,4 +1,27 @@
 package _06_Contravariant;
-
+import _02_GenericClass.MyData;
+/*
+Contravariant artinya kita bisa melakukan subtitusi supertype (parent) dengan subtype (child)
+Caranya agar generic object kita menjadi contravariant adalah dengan menggunakan kata kunci (? super SubClass)
+Artinya saat kita membuat object Contoh<Object>, maka bisa disubtitusi menjadi Contoh<? super String>
+Contravariant adalah bisa write dan read, namun perlu berhati-hati ketika melakukan read, terutama jika sampai parent nya punya banyak child
+ */
 public class ContravariantApp {
+    static void main(String[] args) {
+        MyData<Object> objectMyData = new MyData<>("Permana");
+        objectMyData.setData(1000);
+
+        MyData<? super String> myData = objectMyData;
+
+        process(objectMyData);
+
+        System.out.println(objectMyData.getData());
+    }
+
+    public static void process(MyData<? super String> myData){
+        Object value = myData.getData();
+        System.out.println("Process parameter " + value);
+
+        myData.setData("Permana Surya");
+    }
 }
