a18ecbc75fea25bdc54d30c44f53fa43e8d92930
Permana <sdharma721@gmail.com>
2026-03-16 19:16:41 +0700

    `feat(MyData): Introduce generic class with type parameter T`

--- Java Generic/src/_02_GenericClass/MyData.java
+++ Java Generic/src/_02_GenericClass/MyData.java
@@ -1,4 +1,17 @@
 package _02_GenericClass;
 
-public class MyData {
+public class MyData<T> {
+    private T data;
+
+    public MyData(T data){
+        this.data = data;
+    }
+
+    public T getData(){
+        return data;
+    }
+
+    public void setData(T data){
+        this.data = data;
+    }
 }
