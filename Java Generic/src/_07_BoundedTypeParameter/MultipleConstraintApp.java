414c32376f99f5da5952d24b7227cce35cb95c13
Permana <sdharma721@gmail.com>
2026-03-16 23:22:53 +0700

    feat(bounded-type): Implement multiple bounded type parameters in generic class

--- Java Generic/src/_07_BoundedTypeParameter/MultipleConstraintApp.java
+++ Java Generic/src/_07_BoundedTypeParameter/MultipleConstraintApp.java
@@ -1,5 +1,48 @@
 package _07_BoundedTypeParameter;
+/*
+Kadang kita ingin membatasi tipe data dengan beberapa jenis tipe data di generic parameter type
+Kita bisa menambahkan beberapa bounded type parameter dengan karakter & setelah bounded type pertama
+Jika ingin menambahkan lagi, cukup gunakan karakter & diikuti bounded type nya lagi
+ */
+public class MultipleConstraintApp {
+    static void main(String[] args) {
+        // Data<Manager> managerData = new Data<>(new Manager()); ERROR Manager tidak implement CanSayHello
+        Data<VicePresident> vicePresidentData = new Data<>(new VicePresident());
+    }
 
-public class MultipleConstraint {
-    
+    public static interface CanSayHello {
+        void sayHello(String name);
+    }
+
+    public static abstract class Employee {
+
+    }
+
+    public static class Manager extends Employee {
+
+    }
+
+    public static class VicePresident extends Employee implements CanSayHello {
+
+        @Override
+        public void sayHello(String name) {
+            System.out.println("Hello " + name);
+        }
+    }
+
+    public static class Data<T extends Employee & CanSayHello>{
+        private T data;
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Data(T data) {
+            this.data = data;
+        }
+    }
 }
