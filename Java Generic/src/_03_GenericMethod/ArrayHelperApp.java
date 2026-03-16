c364190e57c81f476217b0020a043910c008d850
Permana <sdharma721@gmail.com>
2026-03-16 23:22:26 +0700

    feat(arrayhelper): introduce count method for arrays

--- Java Generic/src/_03_GenericMethod/ArrayHelperApp.java
+++ Java Generic/src/_03_GenericMethod/ArrayHelperApp.java
@@ -2,6 +2,10 @@ package _03_GenericMethod;
 
 public class ArrayHelperApp {
     static void main(String[] args) {
+        String[] names = {"Permana", "Surya", "Dharma"};
+        Integer[] numbers = {1, 2, 3, 4, 5};
 
+        System.out.println(ArrayHelper.count(names));
+        System.out.println(ArrayHelper.count(numbers));
     }
 }
