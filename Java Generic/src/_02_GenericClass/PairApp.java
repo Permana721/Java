cc10d0ae53628279b069c5652a1f6fb7e9ef601b
Permana <sdharma721@gmail.com>
2026-03-16 19:16:50 +0700

    feat(PairApp): added main method for testing generic Pair class

--- Java Generic/src/_02_GenericClass/PairApp.java
+++ Java Generic/src/_02_GenericClass/PairApp.java
@@ -1,4 +1,9 @@
 package _02_GenericClass;
 
 public class PairApp {
+    static void main(String[] args) {
+        Pair<String, Integer> pair = new Pair<>("Permana", 100);
+        System.out.println(pair.getFirst());
+        System.out.println(pair.getSecond());
+    }
 }
