96899bbba7d36f36e7d8677822e3fc8bff6bea7d
Permana <sdharma721@gmail.com>
2026-03-16 23:22:41 +0700

    refactor(covariant): Implement covariant in MyData generic class and demonstrate usage in CovariantApp.java

--- Java Generic/src/_05_Convariant/CovariantApp.java
+++ Java Generic/src/_05_Convariant/CovariantApp.java
@@ -1,4 +1,22 @@
 package _05_Convariant;
-
+import _02_GenericClass.MyData;
+/*
+Covariant artinya kita bisa melakukan subtitusi subtype (child) dengan supertype (parent)
+Caranya agar generic object kita menjadi covariant adalah dengan menggunakan kata kunci (? extends ParentClass)
+Artinya saat kita membuat object Contoh<String>, maka bisa disubtitusi menjadi Contoh<? extends Object>
+Covariant adalah read-only, jadi kita tidak bisa mengubah data generic nya
+ */
 public class CovariantApp {
+    static void main(String[] args) {
+        MyData<String> stringMyData = new MyData<>("Permana");
+        process(stringMyData);
+
+        MyData<? extends Object> myData = stringMyData;
+    }
+
+    public static void process(MyData<? extends Object> myData){
+        System.out.println(myData.getData());
+
+        // myData.setData("Permana"); tidak boleh, karena berbahaya
+    }
 }
