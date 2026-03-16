2ee12f10daa617bf49c5996cece1ef7c87261f0d
Permana <sdharma721@gmail.com>
2026-03-16 23:22:19 +0700

    refactor(ArrayHelper): Introduce generic method for array length calculation

--- Java Generic/src/_03_GenericMethod/ArrayHelper.java
+++ Java Generic/src/_03_GenericMethod/ArrayHelper.java
@@ -1,4 +1,12 @@
 package _03_GenericMethod;
-
+/*
+Generic parameter type tidak hanya bisa digunakan pada class atau interface
+Kita juga bisa menggunakan generic parameter type di method
+Generic parameter type yang kita deklarasikan di method, hanya bisa diakses di method tersebut, tidak bisa digunakan di luar method
+Ini cocok jika kita ingin membuat generic method, tanpa harus mengubah deklarasi class
+ */
 public class ArrayHelper {
+    public static <T> int count(T[] array) {
+        return array.length;
+    }
 }
