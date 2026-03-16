7508dfd320961645b4cbb08ea2148a53e5472bcd
Permana <sdharma721@gmail.com>
2026-03-16 23:22:59 +0700

    refactor(wildcard): implement wildcard in WildcardApp to print data of any type

--- Java Generic/src/_08_Wildcard/WildcardApp.java
+++ Java Generic/src/_08_Wildcard/WildcardApp.java
@@ -1,4 +1,20 @@
 package _08_Wildcard;
-
+import _02_GenericClass.MyData;
+import _07_BoundedTypeParameter.MultipleConstraintApp;
+/*
+Kadang ada kasus kita tidak peduli dengan generic parameter type pada object
+Misal kita hanya ingin mem-print data T, tidak peduli tipe apapun
+Jika kita mengalami kasus seperti ini, kita bisa menggunakan wildcard
+Wildcard bisa dibuat dengan mengganti generic parameter type dengan karakter ?
+ */
 public class WildcardApp {
+    static void main(String[] args) {
+        print(new MyData<Integer>(100));
+        print(new MyData<String>("Permana"));
+        print(new MyData<MultipleConstraintApp.Manager>(new MultipleConstraintApp.Manager()));
+    }
+
+    public static void print(MyData<?> myData){
+        System.out.println(myData.getData());
+    }
 }
