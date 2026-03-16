b58434b9ba5e9014b51df33f32db03efa763c7be
Permana <sdharma721@gmail.com>
2026-03-16 19:15:45 +0700

    feat(generic): added generic data examples for MyData class in GenericClassApp.java

--- Java Generic/src/_02_GenericClass/GenericClassApp.java
+++ Java Generic/src/_02_GenericClass/GenericClassApp.java
@@ -1,4 +1,14 @@
 package _02_GenericClass;
 
 public class GenericClassApp {
+    static void main(String[] args) {
+        MyData<String> stringMyData = new MyData<String>("Permana");
+        MyData<Integer> integerMyData = new MyData<Integer>(10);
+
+        String stringValue = stringMyData.getData();
+        String integerValue = String.valueOf(integerMyData.getData());
+
+        System.out.println(stringValue);
+        System.out.println(integerValue);
+    }
 }
