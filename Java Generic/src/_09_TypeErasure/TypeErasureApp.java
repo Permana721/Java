5b379ffa561e829311a84e1001debb00c1de8c53
Permana <sdharma721@gmail.com>
2026-03-16 23:23:08 +0700

    fix(typeerasure): implements type erasure in TypeErasureApp with MyData

--- Java Generic/src/_09_TypeErasure/TypeErasureApp.java
+++ Java Generic/src/_09_TypeErasure/TypeErasureApp.java
@@ -1,4 +1,15 @@
 package _09_TypeErasure;
-
+import _02_GenericClass.MyData;
+/*
+Type erasure adalah proses pengecekan generic pada saat compile time, dan menghiraukan pengecekan pada saat runtime
+Type erasure menjadikan informasi generic yang kita buat akan hilang ketika kode program kita telah di compile menjadi binary file
+Compiler akan mengubah generic parameter type menjadi tipe Object di Java
+ */
 public class TypeErasureApp {
+    static void main(String[] args) {
+        MyData myData = new MyData("Permana");
+
+        MyData<Integer> integerMyData = (MyData<Integer>) myData;
+        Integer integer = integerMyData.getData();
+    }
 }
