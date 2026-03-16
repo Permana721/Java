a3b5f2f29b0f47c1b2dbd84d07f42a4ff21eb1f0
Permana <sdharma721@gmail.com>
2026-03-16 23:23:12 +0700

    feat(ComparableApp): Implemented sorting of Person objects using Comparable interface

--- Java Generic/src/_10_ComparableInterface/ComparableApp.java
+++ Java Generic/src/_10_ComparableInterface/ComparableApp.java
@@ -1,4 +1,17 @@
 package _10_ComparableInterface;
 
+import java.util.Arrays;
+
 public class ComparableApp {
+    static void main(String[] args) {
+        Person[] people = {
+                new Person("Permana", "Indonesia"),
+                new Person("Arido", "Indonesia"),
+                new Person("Shifuka", "Jepang")
+        };
+
+        Arrays.sort(people);
+
+        System.out.println(Arrays.toString(people));
+    }
 }
