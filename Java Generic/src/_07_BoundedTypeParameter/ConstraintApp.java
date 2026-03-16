939932997d8d786d6acd97f8d6f9a5fec6ae4d7a
Permana <sdharma721@gmail.com>
2026-03-16 23:22:49 +0700

    refactor(bounded_type_parameter): added constraint to NumberData generic parameter type, now accepting only Number types and their subtypes

--- Java Generic/src/_07_BoundedTypeParameter/ConstraintApp.java
+++ Java Generic/src/_07_BoundedTypeParameter/ConstraintApp.java
@@ -1,4 +1,31 @@
 package _07_BoundedTypeParameter;
+/*
+Kadang kita ingin membatasi data yang boleh digunakan di generic parameter type
+Kita bisa menambahkan constraint di generic parameter type dengan menyebutkan tipe yang diperbolehkan
+Secara otomatis, type data yang bisa digunakan adalah type yang sudah kita sebutkan, atau class-class turunannya
+Secara default, constraint type untuk generic parameter type adalah Object, sehingga semua tipe data bisa digunakan
+ */
+public class ConstraintApp {
+    static void main(String[] args) {
+        NumberData<Integer> integerNumberData = new NumberData<>(1);
+        NumberData<Long> longNumberData = new NumberData<>(1L);
 
-public class ConstraintAp {
+        // NumberData<String> stringNumberData = new NumberData<String>("Permana"); ERROR
+    }
+
+    public static class NumberData<T extends Number>{
+        private T data;
+
+        public NumberData(T data){
+            this.data = data;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+    }
 }
