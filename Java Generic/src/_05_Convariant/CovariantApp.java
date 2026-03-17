1d61fe96d208d0d4c1a78cb3cbf9d1d5fb7567e8
Permana <sdharma721@gmail.com>
2026-03-17 21:11:41 +0700

    refactor(covariant): remove dangerous method setData in MyData generic class

--- Java Generic/src/_05_Convariant/CovariantApp.java
+++ Java Generic/src/_05_Convariant/CovariantApp.java
@@ -5,7 +5,7 @@ Covariant artinya kita bisa melakukan subtitusi subtype (child) dengan supertype
 Caranya agar generic object kita menjadi covariant adalah dengan menggunakan kata kunci (? extends ParentClass)
 Artinya saat kita membuat object Contoh<String>, maka bisa disubtitusi menjadi Contoh<? extends Object>
 Covariant adalah read-only, jadi kita tidak bisa mengubah data generic nya
- */
+*/
 public class CovariantApp {
     static void main(String[] args) {
         MyData<String> stringMyData = new MyData<>("Permana");
