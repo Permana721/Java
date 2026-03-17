c56fb2047f5d05b1c97fa7975c5d8544847a7c49
Permana <sdharma721@gmail.com>
2026-03-17 21:11:07 +0700

    "refactor(generic): improve readability with generic class description"

--- Java Generic/src/_02_GenericClass/GenericClassApp.java
+++ Java Generic/src/_02_GenericClass/GenericClassApp.java
@@ -1,5 +1,12 @@
 package _02_GenericClass;
-
+/*
+Generic adalah kemampuan menambahkan parameter type saat membuat class atau method
+Berbeda dengan tipe data yang biasa kita gunakan di class di function,
+generic memungkinkan kita bisa mengubah-ubah bentuk tipe data sesuai dengan yang kita mau.
+Manfaat Generic : Pengecekan ketika proses kompilasi
+Tidak perlu manual menggunakan pengecekan tipe data dan konversi tipe data
+Memudahkan programmer membuat kode program yang generic sehingga bisa digunakan oleh berbagai tipe data
+ */
 public class GenericClassApp {
     static void main(String[] args) {
         MyData<String> stringMyData = new MyData<String>("Permana");
