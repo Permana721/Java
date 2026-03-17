1a0afb366aefc115087c89c2b0c60f95087fed90
Permana <sdharma721@gmail.com>
2026-03-17 21:11:32 +0700

    refactor(PairApp): Swap generic types in Pair constructor

--- Java Generic/src/_02_GenericClass/PairApp.java
+++ Java Generic/src/_02_GenericClass/PairApp.java
@@ -2,7 +2,7 @@ package _02_GenericClass;
 
 public class PairApp {
     static void main(String[] args) {
-        Pair<String, Integer> pair = new Pair<>("Permana", 100);
+        Pair<Integer, String> pair = new Pair<>(100, "Permana");
         System.out.println(pair.getFirst());
         System.out.println(pair.getSecond());
     }
