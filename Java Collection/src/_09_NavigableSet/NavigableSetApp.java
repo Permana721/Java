992dc455bb8fe274b6f344aea9b447a00a4ce7a2
Permana <sdharma721@gmail.com>
2026-04-05 14:09:47 +0700

    feat(NavigableSet): Implement and demonstrate usage of NavigableSet in Java

--- Java Collection/src/_09_NavigableSet/NavigableSetApp.java
+++ Java Collection/src/_09_NavigableSet/NavigableSetApp.java
@@ -1,4 +1,25 @@
 package _09_NavigableSet;
-
+import java.util.Collections;
+import java.util.NavigableSet;
+import java.util.Set;
+import java.util.TreeSet;
+/*
+NavigableSet adalah turunan dari SortedSet
+NavigableSet menambah method-method untuk melakukan navigasi pencarian element, seperti mencari elemen yang lebih besar dari, kurang dari, membalikkan urutan set, dan lain-lain
+ */
 public class NavigableSetApp {
+    static void main(String[] args) {
+        NavigableSet<String> names = new TreeSet<>();
+        names.addAll(Set.of("Permana", "Surya", "Dharma", "Arido", "Shifuka", "Japan"));
+
+        NavigableSet<String> namesReverse = names.descendingSet();
+        NavigableSet<String> dharma = names.tailSet("Dharma", true);
+
+        for (var name: dharma){
+            System.out.println(name);
+        }
+
+        NavigableSet<String> immutable = Collections.unmodifiableNavigableSet(names);
+        // immutable.add("Hello"); ERROR Immutable Navigable Set
+    }
 }
