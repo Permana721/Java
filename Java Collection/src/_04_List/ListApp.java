a4e5d7fdb23c8a2cb4355eb5b1b8129cd6e09e8e
Permana <sdharma721@gmail.com>
2026-04-05 14:08:27 +0700

    refactor(list): introduces ArrayList and LinkedList, improves usage examples

--- Java Collection/src/_04_List/ListApp.java
+++ Java Collection/src/_04_List/ListApp.java
@@ -1,4 +1,24 @@
 package _04_List;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class ListApp {
+    static void main(String[] args) {
+        List<String> strings = new ArrayList<>(100);
+        List<String> strings1 = new LinkedList<>();
+
+        strings.add("Permana");
+        strings.add("Surya");
+
+        strings.set(0, "Kurosaki");
+
+        strings.remove(1);
+        System.out.println(strings.get(0));
+
+        for (var value : strings) {
+            System.out.println(value);
+        }
+    }
 }
