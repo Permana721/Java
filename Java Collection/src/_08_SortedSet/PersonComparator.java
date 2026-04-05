484ee804d9ee5d2c483a6c72074930ec1f049f2a
Permana <sdharma721@gmail.com>
2026-04-05 14:09:26 +0700

    feat(SortedSet): Implemented PersonComparator for SortedSet sorting by name

--- Java Collection/src/_08_SortedSet/PersonComparator.java
+++ Java Collection/src/_08_SortedSet/PersonComparator.java
@@ -1,4 +1,14 @@
 package _08_SortedSet;
-
-public class PersonComparator {
+import _05_ImmutableList.Person;
+import java.util.Comparator;
+/*
+SortedSet adalah turunan dari Set, namun di SortedSet elemen-elemen yang dimasukkan kedalam SortedSet akan otomatis diurutkan
+Jika element adalah turunan dari interface Comparable, maka secara otomatis akan diurutkan menggunakan comparable tersebut
+Jika element bukan turunan dari interface Comparable, maka kita bisa menggunakan Comparator untuk memberi tahun si SortedSet bagaimana cara mengurutkan elemen-elemen nya
+ */
+public class PersonComparator implements Comparator<Person> {
+    @Override
+    public int compare(Person o1, Person o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
 }
