a044ad7c8a799092176a2f768711692d81cf0e62
Permana <sdharma721@gmail.com>
2026-04-05 14:09:30 +0700

    refactor(sortedset): introduced unmodifiableSortedSet and reversed TreeSet for immutability

--- Java Collection/src/_08_SortedSet/SortedSetApp.java
+++ Java Collection/src/_08_SortedSet/SortedSetApp.java
@@ -1,4 +1,23 @@
 package _08_SortedSet;
 
+import _05_ImmutableList.Person;
+
+import java.util.Collections;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class SortedSetApp {
+    static void main(String[] args) {
+        SortedSet<Person> people = new TreeSet<>(new PersonComparator().reversed());
+        people.add(new Person("Permana"));
+        people.add(new Person("Surya"));
+        people.add(new Person("Dharma"));
+
+        for (var person: people) {
+            System.out.println(person.getName());
+        }
+
+        SortedSet<Person> sortedSet = Collections.unmodifiableSortedSet(people);
+        // sortedSet.add(new Person("Arido")); Immutable Sorted set
+    }
 }
