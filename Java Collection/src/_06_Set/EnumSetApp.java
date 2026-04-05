3fa5cb5980ca83afb3ca2e917343628b374bc22a
Permana <sdharma721@gmail.com>
2026-04-05 14:09:10 +0700

    feat(EnumSetApp): Implement EnumSet with Gender enumeration, print selected genders

--- Java Collection/src/_06_Set/EnumSetApp.java
+++ Java Collection/src/_06_Set/EnumSetApp.java
@@ -1,4 +1,20 @@
 package _06_Set;
-
+import java.util.EnumSet;
+/*
+EnumSet adalah Set yang elemen datanya harus Enum
+Karena data Enum itu unik, sehingga sangat cocok menggunakan Set dibandingkan List
+ */
 public class EnumSetApp {
+    public static enum Gender {
+        MALE, FEMALE, NOT_MENTION
+    }
+
+    static void main(String[] args) {
+        // EnumSet<Gender> genders = EnumSet.allOf(Gender.class); print semua enum
+        EnumSet<Gender> genders = EnumSet.of(Gender.MALE, Gender.FEMALE); // Print hanya beberapa dari enum
+
+        for (var gender: genders) {
+            System.out.println(gender);
+        }
+    }
 }
