e2e53e6d328c7e3cfb6dda9709327eb6dd6918c0
Permana <sdharma721@gmail.com>
2026-04-05 17:47:54 +0700

    feat(EnumMap): Implement EnumMap with levels as keys and names as values

--- Java Collection/src/_12_Map/EnumHashMapApp.java
+++ Java Collection/src/_12_Map/EnumHashMapApp.java
@@ -1,4 +1,26 @@
 package _12_Map;
-
+import java.util.EnumMap;
+import java.util.Map;
+/*
+EnumMap adalah implementasi Map dimana key nya adalah enum
+Karena data enum sudah pasti unik, oleh karena itu cocok dijadikan key di Map
+Algoritma pendistribusian key dioptimalkan untuk enum, sehingga lebih optimal dibandingkan menggunakan hashCode() method
+ */
 public class EnumHashMapApp {
+    public static enum Level {
+        FREE,
+        STANDARD,
+        PREMIUM,
+        VIP
+    }
+
+    static void main(String[] args) {
+        Map<Level, String> map = new EnumMap<Level, String>(Level.class);
+        map.put(Level.FREE, "Permana");
+        map.put(Level.VIP, "Surya");
+
+        for (var key: map.keySet()){
+            System.out.println(map.get(key));
+        }
+    }
 }
