46f403102e0fa754f244ba87afefddf8616f6ebb
Permana <sdharma721@gmail.com>
2026-04-05 14:08:39 +0700

    feat(immutable-list): introduces unmodifiable list and singleton list, removes mutable list

--- Java Collection/src/_05_ImmutableList/ImmutableApp.java
+++ Java Collection/src/_05_ImmutableList/ImmutableApp.java
@@ -1,4 +1,20 @@
 package _05_ImmutableList;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ImmutableApp {
+    static void main(String[] args) {
+        List<String> one = Collections.singletonList("Satu");
+        List<String> empty = Collections.emptyList();
+
+        List<String> mutable = new ArrayList<>();
+        mutable.add("Permana");
+        mutable.add("Surya");
+        List<String> immutable = Collections.unmodifiableList(mutable);
+
+        List<String> elements = List.of("Permana", "Surya", "Dharma");
+//        elements.add("Arido");
+    }
 }
