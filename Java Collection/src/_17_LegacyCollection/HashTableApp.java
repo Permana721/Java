b5038eda99e5c6f76745f1f17891bd4fe9d3c110
Permana <sdharma721@gmail.com>
2026-04-08 08:44:49 +0700

    refactor(legacycollection): remove unused HashTableApp due to outdated usage and concurrentHashMap availability

--- Java Collection/src/_17_LegacyCollection/HashTableApp.java
+++ Java Collection/src/_17_LegacyCollection/HashTableApp.java
@@ -1,9 +1,12 @@
 package _17_LegacyCollection;
-
 import java.util.Hashtable;
 import java.util.Map;
-
-public class HasgTableApp {
+/*
+HashTable adalah implementasi dari Map yang mirip dengan HashMap
+Sama seperti Vector, semua method di HashTable memiliki kata kunci synchronized, sehingga performanya lebih lambat dibandingkan HashMap
+Dan karena di versi Java baru sudah ada juga ConcurrentHashMap, sehingga penggunaan HashTable sudah jarang sekali ditemui
+ */
+public class HashTableApp {
     static void main(String[] args) {
         Map<String, String> map = new Hashtable<>();
 
