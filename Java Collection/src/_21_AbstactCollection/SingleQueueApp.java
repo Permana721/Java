c18ddf2da758aa79967de9f01e51337553f59b8d
Permana <sdharma721@gmail.com>
2026-04-08 08:45:51 +0700

    feat(queue): added single queue example using SingleQueue class

--- Java Collection/src/_21_AbstactCollection/SingleQueueApp.java
+++ Java Collection/src/_21_AbstactCollection/SingleQueueApp.java
@@ -1,4 +1,22 @@
 package _21_AbstactCollection;
 
+import java.util.Queue;
+
 public class SingleQueueApp {
+    static void main(String[] args) {
+        Queue<String> queue = new SingleQueue<>();
+
+        System.out.println(queue.offer("Permana"));
+        System.out.println(queue.offer("Surya"));
+        System.out.println(queue.offer("Dharma"));
+
+        System.out.println(queue.size());
+
+        System.out.println(queue.peek());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+
+        System.out.println(queue.size());
+    }
 }
