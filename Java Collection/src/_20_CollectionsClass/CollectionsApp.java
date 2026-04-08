f33455b99977008ea02baa3bd67912462ef030bc
Permana <sdharma721@gmail.com>
2026-04-08 08:45:47 +0700

    refactor(collections): added import, reverse, shuffle methods for ArrayList usage in CollectionsApp

--- Java Collection/src/_20_CollectionsClass/CollectionsApp.java
+++ Java Collection/src/_20_CollectionsClass/CollectionsApp.java
@@ -1,4 +1,23 @@
 package _20_CollectionsClass;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+/*
+Collections adalah class yang berisikan utility static method untuk membantu kita menggunakan Java Collection
+Di materi-materi sebelumnya kita sudah bahas beberapa, seperti membuat immutable collection misalnya
+Namun sebenarnya masih ada banyak static method yang bisa kita gunakan di class Collections
+ */
 public class CollectionsApp {
+    static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.addAll(List.of("Permana", "Surya", "Dharma", "Arido", "Shifuka"));
+
+        System.out.println(list);
+
+        Collections.reverse(list);
+        System.out.println(list);
+
+        Collections.shuffle(list);
+        System.out.println(list);
+    }
 }
