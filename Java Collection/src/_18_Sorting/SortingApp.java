dae3c3c733f340e9f96619d62fec677633f212de
Permana <sdharma721@gmail.com>
2026-04-08 08:45:30 +0700

    refactor(!SortingApp): Implement built-in sorting method and reverse sort option in SortingApp

--- Java Collection/src/_18_Sorting/SortingApp.java
+++ Java Collection/src/_18_Sorting/SortingApp.java
@@ -1,4 +1,32 @@
 package _18_Sorting;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+/*
+Sorting atau pengurutan adalah algoritma yang sudah biasa kita lakukan
+Di Java Collection juga sudah disediakan cara untuk melakukan pengurutan, jadi kita tidak perlu melakukan pengurutan secara manual
+Namun perlu diingat, yang bisa di sort hanyalah List, karena Set, Queue, Deque dan Map cara kerjanya sudah khusus, jadi pengurutan hanya bisa dilakukan di List
+ */
 public class SortingApp {
+    static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.addAll(List.of("Permana", "Surya", "Dharma", "Arido", "Shifuka"));
+
+        Collections.sort(list);
+
+        Comparator<String> reverse = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o2.compareTo(o1);
+            }
+        };
+
+        Collections.sort(list, reverse);
+
+        for(var value: list){
+            System.out.println(value);
+        }
+    }
 }
