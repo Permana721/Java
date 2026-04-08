d389b54b90c5adc8af7d65d7c94a1fb6c677ca43
Permana <sdharma721@gmail.com>
2026-04-08 08:45:56 +0700

    refactor(MapDefaultMethodApp): Introduced BiConsumer for map iteration

--- Java Collection/src/_22_DefaultMethod/MapDefaultMethodApp.java
+++ Java Collection/src/_22_DefaultMethod/MapDefaultMethodApp.java
@@ -1,4 +1,22 @@
 package _22_DefaultMethod;
 
-public class MapDefaultMethod {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.BiConsumer;
+
+public class MapDefaultMethodApp {
+    static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+
+        map.put("Permana", "Permana Value");
+        map.put("Surya", "Surya Value");
+        map.put("Dharma", "Dharma Value");
+
+        map.forEach(new BiConsumer<String, String>() {
+            @Override
+            public void accept(String s, String s2) {
+                System.out.println(s + ":" + s2);
+            }
+        });
+    }
 }
