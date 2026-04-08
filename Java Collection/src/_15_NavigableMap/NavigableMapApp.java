39bf826acd333c2c4f461d87394a7e5847610818
Permana <sdharma721@gmail.com>
2026-04-08 08:44:44 +0700

    feat(navigable_map): implemented navigable map with key navigation and descending order

--- Java Collection/src/_15_NavigableMap/NavigableMapApp.java
+++ Java Collection/src/_15_NavigableMap/NavigableMapApp.java
@@ -1,4 +1,35 @@
 package _15_NavigableMap;
-
+import java.util.Collections;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+/*
+NavigableMap adalah turunan dari SortedMap
+Namun NagivableMap memiliki kemampuan navigasi berdasarkan operasi kurang dari, lebih dari dan sejenisnya
+Misal, kita ingin mengambil data yang lebih dari key x atau kurang dari key y, ini bisa dilakukan di NavigableMap
+ */
 public class NavigableMapApp {
+    static void main(String[] args) {
+        NavigableMap<String, String> map = new TreeMap<>();
+
+        map.put("Permana", "Permana");
+        map.put("Surya", "Surya");
+        map.put("Dharma", "Dharma");
+
+        for(var key: map.keySet()){
+            System.out.println(key);
+        }
+
+        System.out.println(map.lowerKey("Permana"));
+        System.out.println(map.higherKey("Permana"));
+
+        NavigableMap<String, String> mapDesc = map.descendingMap();
+        for (var key: mapDesc.keySet()){
+            System.out.println(key);
+        }
+
+        NavigableMap<String, String> empty = Collections.emptyNavigableMap();
+        NavigableMap<String, String> immutable = Collections.unmodifiableNavigableMap(mapDesc);
+
+        // immutable.put("Permana", "Permana"); ERROR
+    }
 }
